2b40d2a435201b4dc2c595ae92419a39cd22a227
Amund <amundeb@gmail.com>
2024-04-17 10:08:55 +0200

    Boilerplate for highscore in DB.

---
+++ app/src/main/java/com/example/yatzee/models/Highscore.kt
@@ -0,0 +1,11 @@
+package com.example.yatzee.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Highscore(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val playerName: String = "",
+    val score: Int = 0
+)
